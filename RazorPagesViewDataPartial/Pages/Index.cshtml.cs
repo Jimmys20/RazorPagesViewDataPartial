652ee664f97a4f28b3e6aea97cc8ee9ea6d11c36
jimmys20 <maragkosd12@gmail.com>
2022-08-07 11:25:52 +0300

    Add project files.

---
+++ RazorPagesViewDataPartial/Pages/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPagesViewDataPartial.Models;
+
+namespace RazorPagesViewDataPartial.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public List<Product> Products { get; set; } = new()
+        {
+            new Product { Id = 1, Name = "Product 1" },
+            new Product { Id = 2, Name = "Product 2" },
+            new Product { Id = 3, Name = "Product 3" },
+            new Product { Id = 4, Name = "Product 4" },
+        };
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
